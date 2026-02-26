bfdd97b9addc17f63adc39c38b60351569d364f7
noob <noob@noobs-MacBook-Air.local>
2026-02-26 09:24:38 +0300

    another day another dollar

--- lib/home.dart
+++ lib/home.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+ class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text("My coffee ID"),
+        title:const Text("My coffee ID", style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold
+        ),),
         backgroundColor: Colors.brown[700],
         centerTitle: true,
 
       ),
-      body:const Text('hello'),
+      body:Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+       Container(
+        
+         color: Colors.brown[200],
+         padding: const EdgeInsets.all(20),
+         child: const Text("How i like my coffee....")
+       ),     
+       Container(
+         color: Colors.brown[100],
+         padding: const EdgeInsets.all(20),
+         child: const Text("Coffee prefs.")
+       )
+
+      ],),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,83 @@ import 'package:flutter101/home.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home:Home()
+    // home:Home()
+        home:Sandbox()
+
 )     
   );
 }
 
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title:const  Text("Sandboxing"),
+        backgroundColor: Colors.grey,
+
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+           Container(
+            height: 100,
+            color: Colors.red,
+            child: const Text("one")
+          ),   Container(
+            height: 200,
+            color: Colors.blue,
+            child: const Text("two")
+          ),   Container(
+            height: 300,
+            color: Colors.green,
+            child: const Text("three")
+          )
+        ],
+      )
+    );
+  }
+}
+
+// class Sandbox extends StatelessWidget {
+//   const Sandbox({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return  Scaffold(
+//       appBar: AppBar(
+//         title:const  Text("Sandboxing"),
+//         backgroundColor: Colors.grey,
+
+//       ),
+//       body: Column(
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         mainAxisAlignment: MainAxisAlignment.center,
+
+//         children: [
+//           Container(
+//             width: 100,
+//             color: Colors.red,
+//             child: const Text("one")
+//           ),   Container(
+//             width: 200,
+//             color: Colors.blue,
+//             child: const Text("two")
+//           ),   Container(
+//             width: 300,
+//             color: Colors.green,
+//             child: const Text("three")
+//           ),
+//              Container(
+//             width: 400,
+//             color: Colors.grey,
+//             child: const Text("four")
+//           )
+//         ],
+//       )
+//     );
+//   }
+// }
